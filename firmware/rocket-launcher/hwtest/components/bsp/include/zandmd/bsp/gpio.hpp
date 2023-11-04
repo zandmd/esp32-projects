0b3b028b14aedfb1e1b7049737b5e7be03fee2e9
Miranda Deibert <mirandaldeibert@gmail.com>
2023-11-03 20:11:12 -0500

    made folder structure

--- firmware/rocket-launcher/hwtest/components/bsp/include/zandmd/bsp/gpio.hpp
+++ firmware/rocket-launcher/hwtest/components/bsp/include/zandmd/bsp/gpio.hpp
@@ -3,19 +3,27 @@
 
 #include <zandmd/peripheral-alloc/gpio.hpp>
 
-ZANDMD_GPIO(io0, 0);
-ZANDMD_GPIO(txd, 1);
-ZANDMD_GPIO(led, 2);
-ZANDMD_GPIO(rxd, 3);
-ZANDMD_GPIO(out0, 4);
-ZANDMD_GPIO(miso, 12);
-ZANDMD_GPIO(mosi, 13);
-ZANDMD_GPIO(sclk, 14);
-ZANDMD_GPIO(cs, 15);
-ZANDMD_GPIO(out1, 16);
-ZANDMD_GPIO(btn, 25);
-ZANDMD_GPIO(int1, 26);
-ZANDMD_GPIO(int2, 27);
+ZANDMD_GPIO(btn_c0, 34);
+ZANDMD_GPIO(btn_c1, 35);
+ZANDMD_GPIO(btn_r0, 21);
+ZANDMD_GPIO(btn_r1, 32);
+ZANDMD_GPIO(btn_r2, 33);
+ZANDMD_GPIO(cs, 5);
+ZANDMD_GPIO(fire0, 26);
+ZANDMD_GPIO(fire1, 27);
+ZANDMD_GPIO(fire2, 16);
+ZANDMD_GPIO(fire3, 17);
+ZANDMD_GPIO(led, 14);
+ZANDMD_GPIO(miso, 19);
+ZANDMD_GPIO(mosi, 23);
+ZANDMD_GPIO(rfn_irq, 15);
+ZANDMD_GPIO(rfm_rst,  22);
+ZANDMD_GPIO(sclk, 18);
+ZANDMD_GPIO(sense0, 25);
+ZANDMD_GPIO(sense1, 12);
+ZANDMD_GPIO(sense2, 2);
+ZANDMD_GPIO(sense3, 4);
+ZANDMD_GPIO(vbat, 13);
 
 #include <zandmd/peripheral-alloc/gpio.hpp>
 
--- firmware/rocket-launcher/hwtest/components/bsp/include/zandmd/bsp/peripherals.hpp
+++ firmware/rocket-launcher/hwtest/components/bsp/include/zandmd/bsp/peripherals.hpp
@@ -1,14 +1,12 @@
 #ifndef ZANDMD_BSP_PERIPHERALS_HPP
 #define ZANDMD_BSP_PERIPHERALS_HPP
 
-#include <zandmd/drivers/button.hpp>
 #include <zandmd/drivers/ws2811.hpp>
 
 namespace zandmd {
     namespace bsp {
         class peripherals {
             public:
-                static drivers::button button;
                 static drivers::ws2811 led;
 
             private:
