c1fe41a00e7c96f1ac9157fc0a318eda85f2896c
Zach Deibert <zachdeibert@gmail.com>
2023-12-09 08:15:43 -0600

    Created neopixel-controller firmware project

---
+++ firmware/neopixel-controller/components/bsp/include/zandmd/bsp/gpio.hpp
@@ -0,0 +1,22 @@
+#ifndef ZANDMD_BSP_GPIO_HPP
+#define ZANDMD_BSP_GPIO_HPP
+
+#include <zandmd/peripheral-alloc/gpio_map.hpp>
+
+ZANDMD_GPIO(io0, 0);
+ZANDMD_GPIO(txd, 1);
+ZANDMD_GPIO(data3, 2);
+ZANDMD_GPIO(rxd, 3);
+ZANDMD_GPIO(sda, 12);
+ZANDMD_GPIO(data6, 13);
+ZANDMD_GPIO(scl, 14);
+ZANDMD_GPIO(data5, 15);
+ZANDMD_GPIO(data1, 25);
+ZANDMD_GPIO(data2, 26);
+ZANDMD_GPIO(data4, 27);
+ZANDMD_GPIO(data7, 32);
+ZANDMD_GPIO(data0, 33);
+
+#include <zandmd/peripheral-alloc/gpio_map.hpp>
+
+#endif
