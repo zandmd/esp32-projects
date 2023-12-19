8008bb4990a36958db1f16faa03fbd1b81a4e808
Zach Deibert <zachdeibert@gmail.com>
2023-12-18 21:14:58 -0600

    Updated pattern yet again

--- firmware/neopixel-controller/app/main/src/main.cpp
+++ firmware/neopixel-controller/app/main/src/main.cpp
@@ -15,25 +15,25 @@ extern "C" void app_main() {
     if (xTaskGetTickCount() > pdMS_TO_TICKS(60 * 1000)) { // Weird linker issue
         rtos_init::post_init();
     }
-    constexpr array<multi_span::value_type, 3> colors = {
+    peripherals::lights.all.stripe({
+        multi_span::value_type(255, 0, 0),
+        multi_span::value_type(255, 0, 0),
         multi_span::value_type(255, 0, 0),
         multi_span::value_type(0, 255, 0),
+        multi_span::value_type(0, 255, 0),
+        multi_span::value_type(0, 255, 0),
+        multi_span::value_type(191, 255, 63),
+        multi_span::value_type(191, 255, 63),
         multi_span::value_type(191, 255, 63)
-    };
+    }, 0);
     while (true) {
-        for (const auto &color : colors) {
-            for (size_t i = 0; peripherals::lights.garage_gutter_l2r.color_wipe(color, i, multi_span::center); ++i) {
-                peripherals::lights.update();
-                vTaskDelay(pdMS_TO_TICKS(10));
-            }
-            for (size_t i = 0; peripherals::lights.porch_gutter_l2r.color_wipe(color, i, multi_span::finish); ++i) {
-                peripherals::lights.update();
-                vTaskDelay(pdMS_TO_TICKS(10));
-            }
-            for (size_t i = 0; peripherals::lights.sidewalk_b2f.color_wipe(color, i, multi_span::start); ++i) {
-                peripherals::lights.update();
-                vTaskDelay(pdMS_TO_TICKS(10));
-            }
+        for (size_t i = 0; peripherals::lights.garage_gutter_l2r.color_wipe(multi_span::value_type(255, 0, 0), i, multi_span::start); ++i) {
+            peripherals::lights.update();
+            vTaskDelay(pdMS_TO_TICKS(10));
+        }
+        for (size_t i = 0; peripherals::lights.garage_gutter_l2r.color_wipe(multi_span::value_type(0, 255, 0), i, multi_span::finish); ++i) {
+            peripherals::lights.update();
+            vTaskDelay(pdMS_TO_TICKS(10));
         }
     }
 }
